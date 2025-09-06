1af96b9ea47ce14f22e760d353807b0cb860e337
ranamohyy <ranaamr22@gmail.com>
2025-09-06 16:30:28 +0300

    add all category end point

--- lib/core/networking/app_end_points.dart
+++ lib/core/networking/app_end_points.dart
@@ -2,6 +2,7 @@ class AppEndPoints {
   static const baseUrl = "https://fakestoreapi.com/";
   static const login = "auth/login";
   static const allProducts = "products";
+  static const allCategories = "products/categories";
 
 
 
