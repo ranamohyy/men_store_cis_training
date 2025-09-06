71094d759cbeeb7e6160ee6d7c0473495bc45717
ranamohyy <ranaamr22@gmail.com>
2025-09-06 16:31:12 +0300

    finish get all products

--- lib/features/home_nav/secreens/home_page.dart
+++ lib/features/home_nav/secreens/home_page.dart
@@ -2,59 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:men_store_cis_training/core/helpers/app_colors.dart';
 import 'package:men_store_cis_training/core/helpers/app_text_styles.dart';
+import 'package:men_store_cis_training/features/home_nav/controller/category_cubit/category_cubit.dart';
 import 'package:men_store_cis_training/features/home_nav/controller/product_cubit/product_cubit.dart';
-
+import '../../../core/helpers/app_loading.dart';
+part '../parts/all_categories.dart';
+part '../parts/all_product_home_page.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => ProductCubit()..getProducts(),
-      child: Scaffold(
-        appBar: AppBar(),
-        backgroundColor: AppColors.scaffoldBackGroundColor,
-        body: BlocBuilder<ProductCubit, ProductState>(
-  builder: (context, state) {
-    if(state is ProductLoading){
-      return Center(child: CircularProgressIndicator(),);
-    }
-    if(state is ProductSuccess){
-    return GridView.builder(
-          itemCount: state.model.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 0.8, mainAxisSpacing: 6, crossAxisSpacing: 12
+    return MultiBlocProvider(
+      providers:[
+        BlocProvider(
+        create: (_) => ProductCubit()..getAllProducts(),),
+        BlocProvider(
+        create: (_) => CategoryCubit()..getCategoryProducts(),),
+    ],
+        child: Scaffold(
+          appBar: AppBar(),
+          backgroundColor: AppColors.scaffoldBackGroundColor,
+          body: Column(
+            children: [
+              AllCategories(),
+              AllProductHomePage(),
+            ],
           ),
-          itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(child:
-                  Image.network(state.model[index].image??""),),
-                  Text(
-                    state.model[index].title??"",
-                    style: AppTextStyles.kTextStyle16MediumBlack,
-                    maxLines: 1,),
-                  Text(r"$"+state.model[index].price.toString(),
-                    style: AppTextStyles.kTextStyle16Grey,
-                    textAlign: TextAlign.start,
-                  ),
+        ),
 
-                ],
-              ),);
-          },
-        );
-    }
-    else{
-      return Container( );
-    }
-  },
-),
-      ),
     );
   }
 }
-
