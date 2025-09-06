511e87409a41f552b4a2d73d07fbf4b1d01c8f5b
ranamohyy <ranaamr22@gmail.com>
2025-09-06 16:24:54 +0300

    add colors

--- lib/core/helpers/app_colors.dart
+++ lib/core/helpers/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors{
   static const primaryColor=Color(0xff3669C9);
   static const scaffoldBackGroundColor=Colors.white;
   static const borderFieldColor=Color(0xffE6E6E6);
+  static const greyColor=Color(0xff999999);
 
 
 
