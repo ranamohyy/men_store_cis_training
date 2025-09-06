f53f0a5ac8ca7f04b6562f78f7010f5f306311cb
ranamohyy <ranaamr22@gmail.com>
2025-09-06 16:28:49 +0300

    finish product cubit

--- lib/features/home_nav/controller/product_cubit/product_cubit.dart
+++ lib/features/home_nav/controller/product_cubit/product_cubit.dart
@@ -1,28 +1,37 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:men_store_cis_training/core/networking/app_end_points.dart';
 import 'package:men_store_cis_training/core/networking/dio_helper.dart';
-import 'package:men_store_cis_training/core/networking/end_points.dart';
 import 'package:men_store_cis_training/models/product_model.dart';
 part 'product_state.dart';
 
-class ProductCubit extends Cubit<ProductState> {
+class ProductCubit extends Cubit<ProductStates>{
   ProductCubit() : super(ProductInitial());
 
-  getProducts() async {
-    try {
-      emit(ProductLoading());
-      final Response response =
-          await DioHelper.getRequest(endPoint: AppEndPoints.allProducts);
-      if (response.statusCode == 200) {
-
-        final list = List<ProductModel>.from(
-            (response.data as List).map((e) => ProductModel.fromJson(e)));
-        emit(ProductSuccess(list));
-      }
-    } on DioException {
-      emit(ProductFailure());
-    } catch (e) {
-      emit(ProductFailure());
-    }
+getAllProducts()async{
+  try {
+    emit(ProductLoading());
+  final Response response= await DioHelper.getRequest(endPoint: AppEndPoints.allProducts);
+  if(response.statusCode==200||response.statusCode==201){
+    final list=List<ProductModel>.from((
+    response.data as List
+    ).map((e)=>ProductModel.fromJson(e))
+    );
+    emit(ProductSuccess(list));
   }
+
+  }on DioException catch(e){
+    emit(ProductFailure(e.toString()));
+
+  }
+
+  catch(e){
+    emit(ProductFailure(e.toString()));
+  }
+
+
+}
+
+
+
 }
