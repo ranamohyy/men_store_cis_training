a66af4e417afeda60a02250378bc796d6d486dd6
ranamohyy <ranaamr22@gmail.com>
2025-09-07 13:54:21 +0300

    add styles

--- lib/core/helpers/app_text_styles.dart
+++ lib/core/helpers/app_text_styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:men_store_cis_training/core/helpers/app_colors.dart';
 
 class AppTextStyles{
   static const kHeadTextStyle32=TextStyle(
@@ -12,9 +13,25 @@ class AppTextStyles{
     fontWeight: FontWeight.w500
   );
   static const kTextStyle16MediumBlack=TextStyle(
-    fontSize: 16,
+    fontSize: 20,
     color: Colors.black,
+    fontWeight: FontWeight.w800
+  );
+  static const kTextStyle16MediumGrey=TextStyle(
+    fontSize: 16,
+    color: AppColors.boldGreyColor,
     fontWeight: FontWeight.w500
+  );
+  static const kTextStyle24MediumBlack=TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w700
+  );
+  static const kTextStyle16UnderLineMediumBlack= TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      decoration: TextDecoration.underline
   );
   static const kTextStyle14MediumWhite=TextStyle(
     fontSize: 14,
