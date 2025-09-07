8c17f8842e326b1ea1bdc3597cfd9048af9d4848
ranamohyy <ranaamr22@gmail.com>
2025-09-07 13:57:12 +0300

    start form splash and add easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:men_store_cis_training/core/networking/dio_helper.dart';
-import 'package:men_store_cis_training/features/auth/login/view.dart';
+import 'package:men_store_cis_training/features/splash/view.dart';
 
-void main() {
+
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DioHelper.initDio();
-  runApp(const MyApp());
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en'),
+        startLocale: Locale('en'),
+        child: MyApp()
+    ),
+  );
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }
