6d81d14bf526ee05420e95f7f60574ca56f2eb5f
ranamohyy <ranaamr22@gmail.com>
2025-09-07 12:31:55 +0300

    save user token

--- lib/features/auth/login/cubit.dart
+++ lib/features/auth/login/cubit.dart
@@ -1,48 +1,47 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:men_store_cis_training/core/helpers/cache_helper.dart';
 import 'package:men_store_cis_training/core/networking/dio_helper.dart';
-import 'package:men_store_cis_training/core/networking/end_points.dart';
 import 'package:men_store_cis_training/features/auth/login/state.dart';
-class LoginCubit extends Cubit<LoginStates>{
-  LoginCubit():super(LoginInitial());
-  TextEditingController nameController=TextEditingController();
-  TextEditingController passwordController=TextEditingController();
-  bool obscureText=true;
-  void toggleEye(){
-    obscureText=!obscureText;
+import 'package:men_store_cis_training/models/user_model.dart';
+import '../../../core/networking/app_end_points.dart';
+
+class LoginCubit extends Cubit<LoginStates> {
+  LoginCubit() : super(LoginInitial());
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool obscureText = true;
+  void toggleEye() {
+    obscureText = !obscureText;
     emit(LoginInitial());
   }
-  void login()async{
-    try{
-    emit(LoginLoading());
-     final Response response=await DioHelper.postRequest(
-      endPoint:AppEndPoints.login,
-      data: {
-          "username":nameController.text ,
-          "password": passwordController.text
-        });
-     if(response.statusCode==200||response.statusCode==201){
-       emit(LoginSuccess(response.data.toString()));
-     }
-     else{
-       emit(LoginFailure(response.data.toString()));
-     }
-    }on DioException catch(e){
+
+  void login() async {
+    try {
+      emit(LoginLoading());
+      final Response response = await DioHelper.postRequest(
+          endPoint: AppEndPoints.login,
+          data: {
+            "username": nameController.text,
+            "password": passwordController.text
+          });
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        UserModel userModel = UserModel.fromJson(response.data);
+        if (userModel.token != null) {
+          await CacheHelper.saveToken(userModel.token!);
+        } else {
+          emit(LoginFailure("No token Found"));
+        }
+
+        emit(LoginSuccess(response.data.toString()));
+      } else {
+        emit(LoginFailure(response.data.toString()));
+      }
+    } on DioException catch (e) {
       emit(LoginFailure(e.response!.data.toString()));
-    }
-    catch (e){
+    } catch (e) {
       emit(LoginFailure(e.toString()));
     }
-  
-  
-}
-
-  
-
-
-
-
-
-
+  }
 }
